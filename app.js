931d699cd533bd6bd2f1516891a74fe9c46e9fed
kikisiewe@yahoo.com <kiki>
2024-05-29 21:21:58 -0400

    testing manual approval

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('This is a continuous delivery pipeline!'));
+app.get('/', (req, res) => res.send('I am testing manual approval!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
